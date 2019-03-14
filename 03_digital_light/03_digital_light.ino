e5695d2c5708bc1cf2baf695f0c95de4ac9117b8
Plazmer <praw2003@gmail.com>
2019-03-14 12:14:01 +0500

    04

---
+++ 03_digital_light/03_digital_light.ino
@@ -0,0 +1,164 @@
+#TODO!!!
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include "config.h"
+
+/* DEFINE YOUR PARAMS */
+#define mqtt_topic_status     "rtfbest/status"
+#define mqtt_topic_out        "rtfbest/alight/status"
+#define mqtt_topic_out_pwm    "rtfbest/alight/pwm_value"
+#define mqtt_topic_in         "rtfbest/alight/pwm_override"
+#define LED                   D2
+
+long lastMsgMQTT = 0;
+long lastMsgPWM = 0;
+int value = 0;
+int value_PWM = 0;
+bool is_override = false;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+long lastMsg = 0;
+long delayMS = 1000;
+char msg[50];
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(wifi_ssid);
+  Serial.println(wifi_pass);
+  WiFi.begin(wifi_ssid, wifi_pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println(".");
+    WiFi.printDiag(Serial);
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");  
+  
+  if (length > sizeof(msg) - 1) {
+    length = sizeof(msg) - 1;
+  }
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+    msg[i] = (char) payload[i];
+  }  
+  Serial.println();
+  Serial.println(msg);
+
+  value_PWM = atoi(msg);
+  if ( value_PWM > 1023 ) {
+    value_PWM = 0;    
+  } else {
+    lastMsgPWM = millis();
+    is_override = true;
+  }  
+
+  if ( strcmp(topic, mqtt_topic_in) == 0 ) { 
+   
+    if ((char)payload[0] == '0') {
+      digitalWrite(BUILTIN_LED, HIGH); //BUILTIN_LED has pullup resistor, HIGH = OFF, LOW = ON
+    }
+    
+    if ((char)payload[0] == '1') {
+      digitalWrite(BUILTIN_LED, LOW);
+    }
+  }
+
+}
+
+void reconnect() {
+  String clientId = "ESP8266Client-";
+  clientId += String(ESP.getChipId());
+
+  while (!client.connected()) {
+    Serial.println("Attempting MQTT connection...");
+    Serial.print("Client ID:");
+    Serial.print(clientId);
+    Serial.print(mqtt_login);
+    Serial.print(mqtt_pass);
+    if (client.connect(clientId.c_str(), mqtt_login, mqtt_pass)) {  
+      Serial.println("connected");
+      client.publish(mqtt_topic_status, "reconnected");
+      client.subscribe(mqtt_topic_in);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);  
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  if ( is_override ) {
+    if ( millis() > lastMsgPWM + 10*1000 ) { //timeout 10 second
+      is_override = false; 
+    } else {
+      analogWrite(LED, value_PWM);
+      sprintf (msg, "%i", value_PWM);
+      client.publish(mqtt_topic_out, msg);
+      Serial.print("OVERIDE ");
+      Serial.println(msg);
+  
+    }
+  } else {
+    if ( millis() > lastMsgMQTT + delayMS) {
+      value = analogRead(A0);
+      sprintf (msg, "%i", value);
+      client.publish(mqtt_topic_out, msg);
+      
+      Serial.print("LIGHT ");
+      Serial.println(msg);
+  
+      lastMsgMQTT = millis();
+  
+      value = 1024-value;
+      sprintf (msg, "%i", value);
+      Serial.print("PWM ");
+      Serial.print(msg);
+      Serial.print("/");
+      Serial.println(value_PWM);
+  
+      client.publish(mqtt_topic_out_pwm, msg);      
+      analogWrite(LED, value);    
+    }
+  }  
+
+  long now = millis();
+  if (now - lastMsg > 30000) {    //TODO: overflow long in 49 days
+    lastMsg = now;
+    ++value;
+    sprintf(msg, "status #%ld", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish(mqtt_topic_status, msg);
+  }
+}
--- instructions/04_rgb_led.md
+++ instructions/04_rgb_led.md
@@ -1,1 +1,76 @@
 # RGB-светодиоды
+
+В качестве основы для построения умной лампы будем использовать адресные светодиоды WS2812B (Neopixel).
+
+![](img/WS2812.jpg)
+
+![](img/04/ws2812_module.png)
+
+Для удобства работы провода уже распаяны и закреплены термоусадочной трубкой. Черный провод - GND, красный провод - +5 V (используем цветовую маркировку блоков питания ATX :), желтый - сигнальный - DATA.
+
+
+
+Логика работы простая - все адресные светодиоды кроме собственно RGB светодиода содержат контроллер, который получает поток данных, обрабатывает его и направляет дальше. Обычно они объединяются в цепочку друг за другом.
+
+Описание протокола работы из Datasheet:
+
+![](img/04/ws2812_signal_send.png)
+
+Логика обработки - по сигнальному проводу отправляется цепочка байт. Каждые 3 байта - это компоненты RGB цветового сигнала. Первый адресный контроллер вырезает и использует первые три байта из направленного ему потока данных, направляя дальше остальной поток без изменений. Соответственно на уровне программы следует заранее знать, сколько светодиодов будет в цепочке.
+
+Библиотека <https://github.com/adafruit/Adafruit_NeoPixel> содержит всё необходимое для работы.
+
+![](img/04/04_rgb_led_schema.png)
+
+![](img/04/04_rgb_led_bb.png)
+
+Пример готового устройства. Конденсатор используется для повышения стабильности по питанию, резистор для ограничения силы тока, а зелёный провод ничего не соединяет, просто было удобно закрепить :)
+
+![](img/04/04_rgb_led.png)
+
+
+Пример кода для работы:
+
+```c++
+#include <Adafruit_NeoPixel.h>
+
+#define PIN            D2
+#define NUMPIXELS      8
+#define BRIGHTNESS     50
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+int delayval = 50;
+int r,g,b,j  = 0;
+
+void setup() {
+  strip.begin(); // This initializes the NeoPixel library.
+}
+
+void loop() {
+  colorWipe(strip.Color(0, 0, 0), 50);    // Black/off
+  colorWipe(strip.Color(255, 0, 0), 50);  // Red
+  colorWipe(strip.Color(0, 255, 0), 50);  // Green
+  colorWipe(strip.Color(0, 0, 255), 50);  // Blue
+
+  delay(100);
+}
+
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+```
+
+Задача: 
+
+Задача - написать прошивку, которая будет зажигать все светодиоды одним цветом, полученным из топика **rtfbest/rgb_lamp**  (subscribe)
+
